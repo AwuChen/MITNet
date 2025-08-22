c3788dd751edfb926de33dccd43ffd033a68e09b
Awu Chen <awu.mxr@gmail.com>
2025-08-22 07:28:58 -0400

    Updates

---
+++ precache-manifest.efcb9e4dfec7b66b3dd3e32bbd33dbfe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2b0cfcfcb8e739e00f76e5b453ad072",
+    "url": "/MITNet/index.html"
+  },
+  {
+    "revision": "ec1cf5bd259b8b680043",
+    "url": "/MITNet/static/css/main.01b6fc35.chunk.css"
+  },
+  {
+    "revision": "9a2de3a97dc19a2e66fb",
+    "url": "/MITNet/static/js/2.fe9edaff.chunk.js"
+  },
+  {
+    "revision": "7b36c4a4b00d4c1eb1f21a15ccb34795",
+    "url": "/MITNet/static/js/2.fe9edaff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec1cf5bd259b8b680043",
+    "url": "/MITNet/static/js/main.aa7dac6b.chunk.js"
+  },
+  {
+    "revision": "1bfeb8bf983f20780080",
+    "url": "/MITNet/static/js/runtime-main.5bc9cb6c.js"
+  }
+]);
